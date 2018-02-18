3d368bd49ebcc3a08be1db67b340497364fa85b5
Daniel <dordarus@gmail.com>
2018-02-18 20:14:21 +0200

    Adding versioning

---
+++ app/controllers/v1/books_controller.rb
@@ -0,0 +1,46 @@
+module V1
+    class BooksController < ApplicationController
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
+    skip_before_action :authorize_request, only: [:index, :show] 
+    
+    # GET /books
+    def index
+        @books = Book.all      
+        json_response(@books)  
+    end
+
+    # GET /books/:id
+    def show    
+        json_response(@book)    
+    end
+
+    # POST /books
+    def create
+        @book = Book.create!(book_params)
+        @book.save
+        json_response(@book, :created)  
+    end
+
+    # PATCH /books/:id
+    def update
+        @book.update(book_params)
+        head :no_content
+    end 
+
+    # DELETE /books/:id
+    def destroy
+        @book.destroy
+        head :no_content
+    end
+
+    private
+
+    def book_params
+        params.permit(:title, :genre, :year, :plot, :author_id)
+    end
+
+    def find_book
+        @book = Book.find(params[:id])
+    end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do 
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+
+    #authors routes
+    get '/authors'=>'authors#index', as: :authors_index
+    post '/authors'=>'authors#create'
+    get '/authors/:id' => 'authors#show', as: :author
+    patch '/authors/:id' => 'authors#update'
+    delete '/authors/:id' => 'authors#destroy'
+    #books routes
+    get '/books' => 'books#index', as: :books_index
+    post '/books'=>'books#create'
+    get '/books/:id'=> 'books#show', as: :book
+    patch '/books/:id' => 'books#update'
+    delete '/books/:id' => 'books#destroy'
+  end
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
-
-  #authors routes
-  get '/authors'=>'authors#index', as: :authors_index
-  post '/authors'=>'authors#create'
-  get '/authors/:id' => 'authors#show', as: :author
-  patch '/authors/:id' => 'authors#update'
-  delete '/authors/:id' => 'authors#destroy'
-  #books routes
-  get '/books' => 'books#index', as: :books_index
-  post '/books'=>'books#create'
-  get '/books/:id'=> 'books#show', as: :book
-  patch '/books/:id' => 'books#update'
-  delete '/books/:id' => 'books#destroy'
 end
